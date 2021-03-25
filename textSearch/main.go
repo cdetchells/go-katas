e84e111d4b5b5e4325f9de0cbaccb1853a3ae949
cdetchells <cdetchells@hotmail.com>
2021-03-25 23:31:53 +0000

    Create textSearch kata

---
+++ textSearch/main.go
@@ -0,0 +1,41 @@
+package textSearch
+
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func textSearch(textToSearch, subtext string) string {
+	// If there's no text to search or no subtext then we can just return an empty output
+	if len(textToSearch) == 0 || len(subtext) == 0 {
+		return ""
+	}
+	// Used to store the positions of the matched strings
+	var matches []string
+	// Used to count up the number of matched characters
+	j := 0
+	for i := 0; i < len(textToSearch); i++ {
+		// We have a case sensitive character match
+		if textToSearch[i] == subtext[j] {
+			j++
+		} else {
+			// We have a case insensitive character match
+			if unicode.ToLower(rune(textToSearch[i])) == unicode.ToLower(rune(subtext[j])) {
+				j++
+			} else {
+				// No character match so reset back to 0
+				j = 0
+			}
+		}
+		// We have a match
+		if j == len(subtext) {
+			// Store the start position
+			matches = append(matches, strconv.Itoa((i+1)-(j-1)))
+			// Reset back to 0
+			j = 0
+		}
+	}
+	// Return the array as a comma separated string
+	return strings.Join(matches, ",")
+}
